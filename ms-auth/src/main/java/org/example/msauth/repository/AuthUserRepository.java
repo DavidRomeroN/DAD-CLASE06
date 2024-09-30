f72f8483bc74330d3c8fe2c70a31847389e3595f
davidnina658@gmail.com <davidnina658@gmail.com>
2024-09-30 09:52:34 -0500

    seguridad

--- ms-auth/src/main/java/org/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/org/example/msauth/repository/AuthUserRepository.java
@@ -2,12 +2,13 @@ package org.example.msauth.repository;
 
 import org.example.msauth.entity.AuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
-    Optional<AuthUser> findByUsername(String username);
+    Optional<AuthUser> findByUserName(String username);
 }
 
 
